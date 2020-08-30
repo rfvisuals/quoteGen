fced62ceeecd4f19e000a0a9dfbeb83770380b29
worldpeer <contact@worldpeer.com>
2020-08-29 20:59:15 -0700

    adds button functionality

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authText = document.getElementById('author');
+const twitterBtn= document.getElementById('twitter');
+const newQuoteBtn= document.getElementById('new-quote');
 // GET QUOTE FROM API
 async function getQuote() {
   //Bypass CORS error
@@ -6,11 +11,35 @@ async function getQuote() {
   try {
     const res = await fetch(proxyUrl + apiUrl);
     const data = await res.json();
-    console.log(data);
+    console.log(data)
+    // Add "UNKNOWN" if Author is null
+    if(data.quoteAuthor === "") {
+      authText.innerText = "Unknown"
+    } else {
+      authText.innerText = data.quoteAuthor;
+    }
+    //REDUCE FONT SIZE FOR LONG QUOTES
+    if(data.quoteText.length > 40) {
+      quoteText.classList.add('long-quote');
+    } else {
+      quoteText.classList.remove('long-quote');
+    }
+    quoteText.innerText = data.quoteText;
   } catch(error) {
     getQuote();
     console.log("No quote", error);
   }
 }
+//Tweet Quote
+const tweetQuote = function() {
+  const quote = quoteText.innerText;
+  const author = authText.innerText;
+  const twitterUrl = `https://twitter.com/intent/tweet?text=${quote} -${author}`;
+  window.open(twitterUrl, '_blank')
+}
+
+//EVENT LISTENERS
+newQuoteBtn.addEventListener('click', getQuote);
+twitterBtn.addEventListener('click', tweetQuote)
 
 getQuote();
